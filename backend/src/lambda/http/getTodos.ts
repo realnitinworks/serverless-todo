f2f15e1189967aab757885a01f798d002cd75768
realnitinworks <realnitinworks@gmail.com>
2020-12-02 07:02:30 +0530

    feat: Implement Lambda for getting todos for a user

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,9 +1,25 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log("Processing event: ", event);
+
+  const userId = "123"; // Hard-code for now
+  const result = await docClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  }).promise();
+
+  const items = result.Items;
 
   return {
     statusCode: 200,
@@ -11,7 +27,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items: []
+      items
     })
   }
 }
