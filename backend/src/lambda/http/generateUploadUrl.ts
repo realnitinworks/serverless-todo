a13655b8ded39070da75786405b295b4bef12d7b
realnitinworks <realnitinworks@gmail.com>
2020-12-02 21:37:58 +0530

    feat: Implement Lambda to generate signed url for upload

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,18 +1,57 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+});
+const todosTable = process.env.TODOS_TABLE;
+const bucketName = process.env.ATTACHMENTS_S3_BUCKET;
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log("Processing event: ", event);
+  
+  const userId = "123"; // Hard-code for now
   const todoId = event.pathParameters.todoId
-
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  const uploadUrl = generatePreSignedUploadUrl(todoId);
+  await updateAttachmentUrl(userId, todoId);
+  
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      todoId
+      uploadUrl
     })
   }
+}
+
+
+function generatePreSignedUploadUrl(uniqueId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: uniqueId,
+    Expires: urlExpiration
+  })
+}
+
+async function updateAttachmentUrl(userId: string, todoId: string) {
+  const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
+
+  await docClient.update({
+    TableName: todosTable,
+    Key: {
+      userId,
+      todoId
+    },
+    UpdateExpression: "set attachmentUrl = :attachmentUrl",
+    ExpressionAttributeValues: {
+      ":attachmentUrl": attachmentUrl
+    }
+  }).promise();
 }
