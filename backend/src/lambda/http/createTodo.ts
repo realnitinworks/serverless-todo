91265aedaeabb8822f5899a43fecbba1d7ea8f67
realnitinworks <realnitinworks@gmail.com>
2020-12-02 08:34:47 +0530

    feat: Implement Lambda to create a todo with request validation

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,20 +1,44 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  console.log("Processing event: ", event);
+  
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const userId = "123"; // Hard-code for now
+  const todoId = uuid.v4();
+  const createdAt = new Date().toISOString();
+  const done = false;
 
-  // TODO: Implement creating a new TODO item
+  const newItem = {
+    userId,
+    todoId,
+    createdAt,
+    ...newTodo,
+    done
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise();
+
+
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      newTodo
+      newItem
     })
   }
 }
