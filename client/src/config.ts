0a9c7208f76a7f46453bfa97e32f2657136a9f55
realnitinworks <realnitinworks@gmail.com>
2020-12-03 11:59:36 +0530

    feat: Add authentication and verify app working from React

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'nmivp2py87'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-south-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-3--4mypu.us.auth0.com',                   // Auth0 domain
+  clientId: 'ZYrmEYXspBFH0ySOmNviiJPRjyFjHNG3',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
