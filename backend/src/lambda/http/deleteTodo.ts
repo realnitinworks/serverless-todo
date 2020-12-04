006773bb8672a0acfdc27306b864661612f98b47
realnitinworks <realnitinworks@gmail.com>
2020-12-04 08:08:19 +0530

    feat: Add more validation for delete and upload

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,7 +2,8 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { getUserId } from "../utils"
-import { deleteTodo } from '../../businessLogic/todos';
+import { deleteTodo, getTodo } from '../../businessLogic/todos';
+import { TodoItem } from '../../models/TodoItem'
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -10,6 +11,19 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   
   const userId: string = getUserId(event);
   const todoId: string = event.pathParameters.todoId;
+
+  const todo: TodoItem = await getTodo(userId, todoId);
+  if (!todo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        "error": `todo#${todoId} does not exist`
+      })
+    }
+  }
 
   await deleteTodo(userId, todoId);
   
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,14 +2,33 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { getUserId } from "../utils"
-import { generatePreSignedUploadUrl, updateAttachmentUrl } from "../../businessLogic/todos"
+import { 
+  generatePreSignedUploadUrl,
+  updateAttachmentUrl,
+  getTodo
+} from "../../businessLogic/todos"
+import { TodoItem } from '../../models/TodoItem'
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log("Processing event: ", event);
   
   const userId: string = getUserId(event);
-  const todoId: string = event.pathParameters.todoId
+  const todoId: string = event.pathParameters.todoId;
+
+  const todo: TodoItem = await getTodo(userId, todoId);
+  if (!todo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        "error": `todo#${todoId} does not exist`
+      })
+    }
+  }
+
   const uploadUrl: string = generatePreSignedUploadUrl(todoId);
   await updateAttachmentUrl(userId, todoId);
   
